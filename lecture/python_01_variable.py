ce2e0e3ae522aadb407a88f7ec6802b63690efd7
juyour <76831215+juyour@users.noreply.github.com>
2023-09-08 12:51:43 +0900

    Add python_01_variable

---
+++ lecture/python_01_variable.py
@@ -0,0 +1,10 @@
+# 주석(comment) : 단순 메모, 개발 실행x
+# 출력 함수( print)
+# - ()안의 값을 출력
+# - 변수 값 확인 용도 또는 메세지 출력 용도
+
+print("hello python")
+
+# 문자열 타입(String Type)
+# - Python: '', "" -> String
+# - C, Java: ''(Char), ""(String)
